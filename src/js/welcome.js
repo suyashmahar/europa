e01c50417f81a376d63fe253342468d03abb36cc
Julien Gomes Dias <abld@abld.info>
2021-06-12 18:42:13 +0200

    [fix] shortcuts url and update dependencies

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -1,33 +1,33 @@
 'use strict'
 
-const { ipcRenderer, shell } = require('electron');
+const { ipcRenderer, shell } = require('electron')
 
-const winDecorations = require('../js/modules/winDecorations');
- 
+const winDecorations = require('../js/modules/winDecorations')
+
 const recentItemClicked = (e) => {
-  console.log(e.target.textContent);
-  ipcRenderer.send('open-url', e.target.textContent);
+  console.log(e.target.textContent)
+  ipcRenderer.send('open-url', e.target.textContent)
 }
 const deleteRecentItemClicked = (e) => {
-  console.log(e.target.getAttribute('id'));
-  ipcRenderer.send('delete-recent-url', e.target.getAttribute('id'));
+  console.log(e.target.getAttribute('id'))
+  ipcRenderer.send('delete-recent-url', e.target.getAttribute('id'))
 }
 
-function main() {
+function main () {
   document.getElementById('openUrlBtn').addEventListener('click', () => {
-    ipcRenderer.send('open-new-url');
-  });
+    ipcRenderer.send('open-new-url')
+  })
   document.getElementById('newServerBtn').addEventListener('click', () => {
-    ipcRenderer.send('new-server-window');
-  });
+    ipcRenderer.send('new-server-window')
+  })
   document.getElementById('optionsBtn').addEventListener('click', () => {
-    ipcRenderer.send('options-window');
-  });
+    ipcRenderer.send('options-window')
+  })
 
   // Listeners for help links
   document.getElementById('helpReportIssue').addEventListener('click', () => {
     shell.openExternal('https://github.com/suyashmahar/europa/issues/new');
-  });
+  })
   // document.getElementById('helpGitHubRepo').addEventListener('click', () => {
   //   shell.openExternal('https://github.com/suyashmahar/europa');
   // });
@@ -42,25 +42,25 @@ function main() {
   });
   document.getElementById('helpAboutEuropa').addEventListener('click', () => {
     ipcRenderer.send('show-about-europa');
-  });
+  })
 
   ipcRenderer.on('recent-urls', (event, recentUrls) => {
-    const recentUrlElem = document.getElementById('recentUrls');
+    const recentUrlElem = document.getElementById('recentUrls')
 
     /* create an html string */
     var recentUrlHtml = recentUrls.reduce((html, item) => {
       var itemDeleteBtnHtml = `<a id="${item}" href="javascript:void" class="inline-btn-link-delete font-weight-bold">✕</a>`
       html += `<li class="recent-item"><a class="recent-item-link" href="javascript:void" id="${item}">${item}</a>${itemDeleteBtnHtml}</li>`
 
-      return html;
+      return html
     }, '')
 
     if (!recentUrlHtml) {
-      recentUrlHtml = "No recent items";
+      recentUrlHtml = 'No recent items'
     }
 
     /* set list html to the todo items */
-    recentUrlElem.innerHTML = recentUrlHtml;
+    recentUrlElem.innerHTML = recentUrlHtml
 
     /* Add click handlers to the link */
     document.querySelectorAll('.recent-item-link').forEach(item => {
@@ -73,7 +73,7 @@ function main() {
     })
   })
 
-  winDecorations.setupDecorations();
+  winDecorations.setupDecorations()
 }
 
-main();
+main()
